7b8e82b31545c62f64da47ac9eb6021c59e1869f
feed4rz <mrspanky@yandex.ru>
2017-09-02 22:18:25 +0300

    Added chats page

--- controllers/index.js
+++ controllers/index.js
@@ -9,6 +9,10 @@ router.get('/', (req, res) => {
   res.render('index');
 });
 
+router.get('/chats', (req, res) => {
+  res.render('chats');
+});
+
 router.get('/labaccess', (req, res) => {
   res.render('vr_lab_calculator');
 });
