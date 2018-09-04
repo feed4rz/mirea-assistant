0cfcb5ea69273bc5d35c2a28da36b2ebd886d019
feed4rz <mrspanky@yandex.ru>
2018-09-04 09:30:12 +0300

    Remove schedule checkbox

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,41 +1,44 @@
-$(document).ready(function(){
-  document.body.onfocus = function(){
-		$('#xlsx_upload_btn').removeClass('loading');
-	}
+$(document).ready(function() {
+  document.body.onfocus = function() {
+    $('#xlsx_upload_btn').removeClass('loading');
+  }
+
+  $('#remove').checkbox('check')
+
 });
 
-function upload_xlsx(){
+function upload_xlsx() {
   $('#xlsx_upload_btn').removeClass('loading');
   $('#xlsx_upload_btn').addClass('loading');
 
   $('#xlsx_upload').click();
 }
 
-function sheet2arr(sheet){
-   var json = XLSX.utils.sheet_to_json(sheet, { header : 1 });
-   console.log(json);
-   var object = {};
-   var counter = 0;
+function sheet2arr(sheet) {
+  var json = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+  console.log(json);
+  var object = {};
+  var counter = 0;
 
-   for(var i = 0; i < json.length; i++){
-     for(var j = 0; j < json[i].length; j++){
-       if(!object[`FIELD${j}`]) object[`FIELD${j}`] = [];
-       if(!json[i][j]) json[i][j] = '';
-       object[`FIELD${j}`].push(json[i][j].replace(/\r\n/g,' '));
-     }
-   }
+  for(var i = 0; i < json.length; i++) {
+    for(var j = 0; j < json[i].length; j++) {
+      if(!object[`FIELD${j}`]) object[`FIELD${j}`] = [];
+      if(!json[i][j]) json[i][j] = '';
+      object[`FIELD${j}`].push(json[i][j].replace(/\r\n/g, ' '));
+    }
+  }
 
-   return object;
+  return object;
 }
 
 var schedules = [];
 
-function handleXLSX(event){
-	var input = event.target;
+function handleXLSX(event) {
+  var input = event.target;
 
   var reader = new FileReader();
 
-  reader.onload = function(){
+  reader.onload = function() {
     var data = new Uint8Array(reader.result);
 
     var workbook = XLSX.read(data, { type: 'array' });
@@ -57,31 +60,31 @@ function handleXLSX(event){
 
     var group = null;
 
-    for(var key in columns){
+    for(var key in columns) {
       var column = columns[key];
 
-      if(column.indexOf('Предмет') > -1){
+      if(column.indexOf('Предмет') > -1) {
         group = column[column.indexOf('Предмет') - 1];
 
-        for(var i = column.indexOf('Предмет') + 1; i < 73 + column.indexOf('Предмет'); i++){
+        for(var i = column.indexOf('Предмет') + 1; i < 73 + column.indexOf('Предмет'); i++) {
           classes.push(column[i] || '');
         }
       }
 
-      if(column.indexOf('Вид занятий') > -1){
-        for(var i = column.indexOf('Вид занятий') + 1; i < 73 + column.indexOf('Вид занятий'); i++){
+      if(column.indexOf('Вид занятий') > -1) {
+        for(var i = column.indexOf('Вид занятий') + 1; i < 73 + column.indexOf('Вид занятий'); i++) {
           types.push(column[i] || '');
         }
       }
 
-      if(column.indexOf('ФИО преподавателя') > -1){
-        for(var i = column.indexOf('ФИО преподавателя') + 1; i < 73 + column.indexOf('ФИО преподавателя'); i++){
+      if(column.indexOf('ФИО преподавателя') > -1) {
+        for(var i = column.indexOf('ФИО преподавателя') + 1; i < 73 + column.indexOf('ФИО преподавателя'); i++) {
           teachers.push(column[i] || '');
         }
       }
 
-      if(column.indexOf('№  ауд.') > -1){
-        for(var i = column.indexOf('№  ауд.') + 1; i < 73 + column.indexOf('№  ауд.'); i++){
+      if(column.indexOf('№  ауд.') > -1) {
+        for(var i = column.indexOf('№  ауд.') + 1; i < 73 + column.indexOf('№  ауд.'); i++) {
           rooms.push(column[i] || '');
         }
 
@@ -89,11 +92,11 @@ function handleXLSX(event){
 
         var sorted_classes = [];
 
-        for(var i = 0; i < classes.length; i++){
-          if(i % 2 == 0){
+        for(var i = 0; i < classes.length; i++) {
+          if(i % 2 == 0) {
             var current_class = {
-              odd : { name : classes[i] || null, type : convertType(types[i]), teacher : teachers[i] || null, room : rooms[i] || null },
-              even : { name : classes[i+1] || null, type : convertType(types[i+1]), teacher : teachers[i+1] || null, room : rooms[i+1] || null }
+              odd: { name: classes[i] || null, type: convertType(types[i]), teacher: teachers[i] || null, room: rooms[i] || null },
+              even: { name: classes[i + 1] || null, type: convertType(types[i + 1]), teacher: teachers[i + 1] || null, room: rooms[i + 1] || null }
             };
 
             sorted_classes.push(current_class);
@@ -102,15 +105,15 @@ function handleXLSX(event){
 
         var days = [];
 
-        for(var i = 0; i < sorted_classes.length; i++){
-          if(i % 6 == 0){
+        for(var i = 0; i < sorted_classes.length; i++) {
+          if(i % 6 == 0) {
             var day = [
               sorted_classes[i],
-              sorted_classes[i+1],
-              sorted_classes[i+2],
-              sorted_classes[i+3],
-              sorted_classes[i+4],
-              sorted_classes[i+5]
+              sorted_classes[i + 1],
+              sorted_classes[i + 2],
+              sorted_classes[i + 3],
+              sorted_classes[i + 4],
+              sorted_classes[i + 5]
             ];
 
             days.push(day);
@@ -118,10 +121,10 @@ function handleXLSX(event){
         }
 
         var schedule = {
-          institute : institute,
-          group : convertGroupName(group),
-          secret : "S"+Math.floor(Math.random()*1000000),
-          days : days
+          institute: institute,
+          group: convertGroupName(group),
+          secret: "S" + Math.floor(Math.random() * 1000000),
+          days: days
         };
 
         schedules.push(schedule);
@@ -139,29 +142,29 @@ function handleXLSX(event){
   reader.readAsArrayBuffer(input.files[0]);
 }
 
-function convertGroupName(name){
-  name = name.replace(/ /g,'');
+function convertGroupName(name) {
+  name = name.replace(/ /g, '');
   name = translit(name, 5).toLowerCase();
 
   return name;
 }
 
-function convertType(type){
+function convertType(type) {
   var result = "";
 
-  type = type.replace(/ /g,'');
+  type = type.replace(/ /g, '');
 
-  if(type.indexOf('+') > -1){
+  if(type.indexOf('+') > -1) {
     type = type.split('+');
 
     if(type[0] != 'лк') result = Number(convertType(type[0]) + "" + convertType(type[1]));
     if(type[0] == 'лк') result = Number(convertType(type[1]) + "" + convertType(type[0]));
   } else {
     var Types = {
-      "" : null,
-      "лк" : 0,
-      "пр" : 1,
-      "лаб" : 2
+      "": null,
+      "лк": 0,
+      "пр": 1,
+      "лаб": 2
     };
 
     result = Types[type];
@@ -170,62 +173,104 @@ function convertType(type){
   return result;
 }
 
-function upload(){
+function upload() {
   $('#upload_btn').removeClass('loading');
   $('#upload_btn').addClass('loading');
 
   var institute = Number($('#institute').val() || 0);
   var secret = $('#secret').val();
+  var remove = $('#remove').hasClass('checked')
 
-  api.schedule_remove_all({
-    institute : institute,
-    secret : secret
-  }, (err, res) => {
-    if(err){
-      console.log(err);
+  if(remove) {
+    api.schedule_remove_all({
+      institute: institute,
+      secret: secret
+    }, (err, res) => {
+      if(err) {
+        console.log(err);
 
-      $('#upload_btn').removeClass('loading');
-    } else {
-  		console.log('Current schedule: clear');
+        $('#upload_btn').removeClass('loading');
+      } else {
+        console.log('Current schedule: clear');
 
-      api.group_remove_all({
-        institute : institute,
-        secret : secret
-      }, (err, res) => {
-        if(err){
-          console.log(err);
+        api.group_remove_all({
+          institute: institute,
+          secret: secret
+        }, (err, res) => {
+          if(err) {
+            console.log(err);
 
-          $('#upload_btn').removeClass('loading');
-        } else {
-  				console.log('Current group list: clear');
+            $('#upload_btn').removeClass('loading');
+          } else {
+            console.log('Current group list: clear');
 
-          for(var i = 0; i < schedules.length; i++){
-            api.schedule_new({
-              schedule : schedules[i],
-              secret : secret
-            }, (err, res) => {});
+            for(var i = 0; i < schedules.length; i++) {
+              api.schedule_new({
+                schedule: schedules[i],
+                secret: secret
+              }, (err, res) => {});
 
-            var group = {
-              institute : institute,
-              group : schedules[i].group,
-              secret : schedules[i].secret
-            };
+              var group = {
+                institute: institute,
+                group: schedules[i].group,
+                secret: schedules[i].secret
+              };
 
-            api.group_new({
-              group : group,
-              secret : secret
-            }, (err, res) => {
-              if(err){
-                console.log(err);
-              }
-            });
+              api.group_new({
+                group: group,
+                secret: secret
+              }, (err, res) => {
+                if(err) {
+                  console.log(err);
+                }
+              });
+            }
+
+            console.log('Done');
+
+            $('#upload_btn').removeClass('loading');
           }
+        });
+      }
+    });
+  } else {
+    api.group_remove_all({
+      institute: institute,
+      secret: secret
+    }, (err, res) => {
+      if(err) {
+        console.log(err);
 
-  				console.log('Done');
+        $('#upload_btn').removeClass('loading');
+      } else {
+        console.log('Current group list: clear');
 
-          $('#upload_btn').removeClass('loading');
+        for(var i = 0; i < schedules.length; i++) {
+          api.schedule_new({
+            schedule: schedules[i],
+            secret: secret
+          }, (err, res) => {});
+
+          var group = {
+            institute: institute,
+            group: schedules[i].group,
+            secret: schedules[i].secret
+          };
+
+          api.group_new({
+            group: group,
+            secret: secret
+          }, (err, res) => {
+            if(err) {
+              console.log(err);
+            }
+          });
         }
-      });
-    }
-  });
+
+        console.log('Done');
+
+        $('#upload_btn').removeClass('loading');
+      }
+    });
+  }
 }
